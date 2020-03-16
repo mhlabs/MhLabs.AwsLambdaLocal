4c9bd67332f23a8b8851847bccfe2d4048acb0e0
Lars Jacobsson <lars.jacobsson@mathem.se>
2020-03-16 13:33:11 +0100

    feat: sso

--- MhLabs.AWSLambdaLocal/LambdaEnvironment.cs
+++ MhLabs.AWSLambdaLocal/LambdaEnvironment.cs
@@ -7,6 +7,8 @@ using Amazon.CloudFormation;
 using Amazon.CloudFormation.Model;
 using Amazon.Lambda;
 using Amazon.Lambda.Model;
+using Amazon.Runtime;
+using MhLabs.AwsCliSso;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,11 +17,19 @@ namespace MhLabs.AwsLambdaLocal
 {
     public static class LambdaEnvironment
     {
-        public static async Task MapToLocal(RegionEndpoint region, string stackName = null, string apiProxyLogicalId = "ApiProxy")
+        public static async Task MapToLocal(RegionEndpoint region, string stackName = null, string apiProxyLogicalId = "ApiProxy", string accountId = null, string startUrl = null, string roleName = null)
         {
+            AWSCredentials credentials = null;
+            startUrl = startUrl ?? System.Environment.GetEnvironmentVariable("AWS_SSO_START_URL");
+            if (!string.IsNullOrEmpty(startUrl))
+            {
+                var service = new AwsCliSsoService(region);
+                credentials = await service.GetCredentials(startUrl, accountId ?? System.Environment.GetEnvironmentVariable("AWS_SSO_ACCOUNT_ID"), roleName ?? System.Environment.GetEnvironmentVariable("AWS_SSO_LAMBDA_LOCAL_ROLE_NAME"));
+            }
+
             stackName = stackName ?? Assembly.GetEntryAssembly().GetName().Name.Replace('_', '-');
-            var cloudFormation = new AmazonCloudFormationClient(region);
-            var lambdaClient = new AmazonLambdaClient(region);
+            var cloudFormation = new AmazonCloudFormationClient(credentials, region);
+            var lambdaClient = new AmazonLambdaClient(credentials, region);
             var resources =
                 await cloudFormation.DescribeStackResourcesAsync(
                     new DescribeStackResourcesRequest { StackName = stackName });
